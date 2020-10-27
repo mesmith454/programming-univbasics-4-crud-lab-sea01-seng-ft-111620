7681194054202579a9ef13883e3db7072571b68f
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-27 19:26:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  cats = ["snowshoe","tuxedo","persian"]
+  cats = ["snowshoe","tuxedo","persian","tabby"]
 end
 
 def add_element_to_end_of_array(array, element)
