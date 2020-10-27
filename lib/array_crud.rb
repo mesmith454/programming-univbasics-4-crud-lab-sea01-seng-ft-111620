e0ad781e123b6881acf84bc835d7a93fac2df8a1
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-27 19:33:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,19 +1,21 @@
+cats = ["snowshoe","tuxedo","persian","tabby"]
+
 def create_an_empty_array
   my_array = []
 end
 
 def create_an_array
-  cats = ["snowshoe","tuxedo","persian","tabby"]
+  meat = ["turkey","ham","chicken","beef"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(cats, element)
   cats = ["snowshoe","tuxedo","persian","tabby"]
   cats.push("bengal")
 end
 
 def add_element_to_start_of_array(array, element)
-  
-  
+  cats = ["snowshoe","tuxedo","persian","tabby"]
+  cats.unshift = "bengal"
 end
 
 def remove_element_from_end_of_array(array)
