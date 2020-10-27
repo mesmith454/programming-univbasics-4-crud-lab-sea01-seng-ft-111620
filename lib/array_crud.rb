bad888158225f964cbb95eaf07575904dbad2569
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-27 19:28:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   cats = ["snowshoe","tuxedo","persian","tabby"]
   cats << "bengal"
+  puts cats
 end
 
 def add_element_to_start_of_array(array, element)
